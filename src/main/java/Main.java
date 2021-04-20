5b0ae60572dfda3c78c3cb3e72b09c3a1030a385
kienboeck <kienboec@technikum-wien.at>
2021-04-20 09:38:29 +0200

    first version

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    /**
+     * entry point for the application
+     * @param args command line arguments
+     */
+    public static void main(String[] args){
+        // "C:\Users\Daniel Kienbock\.jdks\openjdk-15.0.2\bin\java.exe" -jar C:\repos\MavenSample\target\maven-testing-1.0-SNAPSHOT.jar
+        //
+        // Tools / Generate JavaDoc
+        System.out.println("hello world");
+    }
+}
