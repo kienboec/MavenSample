8b215f3f3dc4f4280f33c1e80d4e89e20c9248b6
kienboeck <kienboec@technikum-wien.at>
2021-04-23 19:04:12 +0200

    added checkstyle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,19 @@
-public class Main {
+public final class Main {
+
     /**
-     * entry point for the application
+     * make this class in-instantiable.
+     */
+    private Main() {
+
+    }
+
+    /**
+     * Entry point for the application.
      * @param args command line arguments
      */
-    public static void main(String[] args){
-        // "C:\Users\Daniel Kienbock\.jdks\openjdk-15.0.2\bin\java.exe" -jar C:\repos\MavenSample\target\maven-testing-1.0-SNAPSHOT.jar
+    public static void main(final String[] args) {
+        // "C:\Users\Daniel Kienbock\.jdks\openjdk-15.0.2\bin\java.exe"
+        //  -jar C:\repos\MavenSample\target\maven-testing-1.0-SNAPSHOT.jar
         //
         // Tools / Generate JavaDoc
         System.out.println("hello world");
---
+++ src/main/java/package-info.java
@@ -0,0 +1,2 @@
+// https://www.baeldung.com/java-package-info
+// this file is created to document the package
