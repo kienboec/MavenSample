f3a365629e8fbdf64638d33d0d3ecea904e761ed
kienboeck <kienboec@technikum-wien.at>
2021-04-23 19:04:42 +0200

    updated checkstyle dependency and added tests

--- src/test/java/mainTest.java
+++ src/test/java/mainTest.java
@@ -1,2 +1,8 @@
-package PACKAGE_NAME;public class mainTest {
+import org.junit.jupiter.api.Test;
+
+public class mainTest {
+    @Test
+    public void test_Given_ApplicationStarted_When_CallingMain_Then_NoExceptionShouldBeThrown() {
+        Main.main(null);
+    }
 }
